605b972bd9a783133a1848755699604ee0a05ded
Beard6580 <44980780+slayer6580@users.noreply.github.com>
2022-01-02 15:45:46 -0500

    Ajout Character
    Pac-man + mouvement
    Ghost Template

---
+++ Test_Pacman/Assets/Scripts/PacmanScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanScript : MonoBehaviour
+{
+	private float moveX;
+	private float moveY;
+
+	[SerializeField] 
+	private float speed;
+	private Vector3 direction;
+	private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input for mouvement
+    	float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        direction = (transform.right * moveX + transform.forward * moveY);
+
+        controller.Move(direction * speed * Time.deltaTime);
+    }
+}
