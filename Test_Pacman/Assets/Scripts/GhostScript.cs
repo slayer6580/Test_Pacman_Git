c61310d1cd587a8141d009277305497084682c3c
Beard6580 <44980780+slayer6580@users.noreply.github.com>
2022-01-02 19:07:30 -0500

    GhostScript - Ajout
    Ajout du mode Wander
    Ajout de Waypoint pour le mode

--- Test_Pacman/Assets/Scripts/GhostScript.cs
+++ Test_Pacman/Assets/Scripts/GhostScript.cs
@@ -17,11 +17,15 @@ public class GhostScript : MonoBehaviour
 	}
 
 	private GameObject player;
+	private GameObject wanderWaypoints;
+	private Vector3 position;
+	private bool goingToWaypoint;
 
 
     void Start()
     {
         player = GameObject.Find("Pacman");
+        wanderWaypoints = GameObject.Find("WanderWaypoints");
     }
 
     void Update()
@@ -43,10 +47,21 @@ public class GhostScript : MonoBehaviour
     }
 
 
-
     private void Wander()
     {
+    	if(goingToWaypoint == false)
+    	{
+    		goingToWaypoint = true;
+    		int _waypointToGo = Mathf.RoundToInt(Random.Range(0, wanderWaypoints.transform.childCount));
+    		position = wanderWaypoints.gameObject.transform.GetChild(_waypointToGo).transform.position;
+    	}
+    	agent.SetDestination(position);
 
+    	if(Vector3.Distance(position, transform.position) <= 1)
+    	{
+			goingToWaypoint = false;
+    	}
+    	
     }
 
     private void Chase()
